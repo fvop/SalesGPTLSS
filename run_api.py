9d50c7170fa7aa1f7295b6bb3948f80ecd095851
fvop <141462158+fvop@users.noreply.github.com>
2025-05-08 00:26:04 +0700

    Update run_api.py

--- run_api.py
+++ run_api.py
@@ -133,5 +133,4 @@ async def chat_with_sales_agent(req: MessageList, stream: bool = Query(False), a
 
 
 # Main entry point
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+# No manual run, Railway handles this
